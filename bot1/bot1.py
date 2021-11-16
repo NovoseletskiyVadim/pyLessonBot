5ab83396c9546fb75446e7d1c44fb024bc83cda8
Vadim <novoseletskiy.v@gmail.com>
2021-11-17 00:34:28 +0200

    Start

--- bot1/bot1.py
+++ bot1/bot1.py
@@ -1,1 +1,16 @@
 
+
+import config
+import telebot
+
+
+bot=telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_allMessages(message):
+
+    bot.send_message(config.GrID, message.text)
+
+
+if __name__=='__main__':
+    bot.infinity_polling()
