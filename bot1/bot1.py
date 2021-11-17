0ebafb4fa8489d1a31e22dd671b7526a784dda4e
Безсонов Руслан <ttm121212@gmail.com>
2021-11-17 17:38:42 +0200

    Testing with Chanels

--- bot1/bot1.py
+++ bot1/bot1.py
@@ -9,7 +9,9 @@ bot=telebot.TeleBot(config.TOKEN)
 @bot.message_handler(content_types=["text"])
 def repeat_allMessages(message):
 
-    bot.send_message(config.GrID, message.text)
+    print("message.chat.id=",message.chat.id)
+
+    bot.send_message(config.Chanel_2, message.text)
 
 
 if __name__=='__main__':
